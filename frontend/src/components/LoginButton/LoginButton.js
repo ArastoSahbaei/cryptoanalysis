621c434f5fe7b1987a21857c236ae4ced92cbd1f
Tomas <cernytomas@hotmail.com>
2021-11-11 00:23:18 +0100

    Validate login password and email
    'react-material-ui-form-validator'
    
    npm install

--- frontend/src/components/LoginButton/LoginButton.js
+++ frontend/src/components/LoginButton/LoginButton.js
@@ -4,30 +4,29 @@ import Popup from "reactjs-popup"
 import { UserContext } from "../../shared/global/provider/UserProvider"
 import CryptoShuttleService from "../../utils/api/services/CryptoShuttleService"
 import {useHistory} from "react-router-dom"
-
+import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator'
 
 export const LoginButton = () => {
- 
+
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
     const history = useHistory();
 
-    
-    
 
     const handleSubmit = async (event)   => {
         event.preventDefault()
 
-       try{
-        const userFromServer = await CryptoShuttleService.loginUser({email, password})
-        setAuthenticatedUser(userFromServer.data.access_token)
-        localStorage.setItem("token", userFromServer.data.access_token)
-        /* document.getElementsById('noMatch').style.visibility = "hidden" */
-        }
-        catch(error){
-            document.getElementById("noMatch").style.visibility = "visible"
-        }        
+      
+          try{
+            const userFromServer = await CryptoShuttleService.loginUser({email, password})
+            setAuthenticatedUser(userFromServer.data.access_token)
+            localStorage.setItem("token", userFromServer.data.access_token)
+            /* document.getElementsById('noMatch').style.visibility = "hidden" */
+            }
+            catch(error){
+                document.getElementById("noMatch").style.visibility = "visible"
+            }           
     }
     
     return (
@@ -35,30 +34,29 @@ export const LoginButton = () => {
             <Popup  trigger={<button className="triggerButton"> Login</button>} modal >
                 {close => (
                     <div className="wrapper">
-                    <form onSubmit={handleSubmit}>
+                    <ValidatorForm  onSubmit={handleSubmit}>
                         <div>Log In</div><br/>
                         <div>
                             New to Crypto Shuttle?
                             <span className="create-account" onClick={() => {close(); history.push('/userregister')}}>Create an account</span>
                         </div><br/>
                         <div>
-                        <label ><b>Email</b></label><br/>
-                        <input onChange={event => setEmail(event.target.value)} type="email" className="email" placeholder="Enter Email" name="email" required></input>
+                        
+                        <TextValidator onChange={event => setEmail(event.target.value)} type="email"  label="Email" value={email} name="email" validators={['required', 'isEmail']} errorMessages={['this field is required', 'email is not valid']} />
                         </div><br/>
                         <div>
-                        <label><b>Password</b></label><br/>
-                        <input onChange={event => setPassword(event.target.value)} type="password" className="password" placeholder="Enter Password" name="psw" required></input>
+                        <TextValidator onChange={event => setPassword(event.target.value)} type="password" label="Password" name="password"  validators={['required', 'matchRegexp:^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$']} errorMessages={['this field is required', 'Minimum eight characters, at least one letter, one number and one special character']} value={password}  />
                         
-                        <div id="noMatch">Your email and password doesnt match. Please try again</div>
+                        <div id="noMatch">Your email or password doesnt match. Please try again</div>
                         </div><br/>
                         <div>
                         <button type="submit" className="submit">Submit</button> 
                         </div>
-                    </form>
+                    </ValidatorForm>
                 </div>
                 )}
                 
             </Popup>        
         </div>
     )
-}
+};
